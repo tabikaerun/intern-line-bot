9322be71cff16a930d1d0fcb32d3a624b4f7f373
gifteeintern-D <gifteeintern-d@giftee-no-MacBook-Pro.local>
2018-10-23 13:50:49 +0900

    detecting image url  Please enter the commit message for your changes. Lines starting

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -24,15 +24,40 @@ class WebhookController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
+          #message from user
           message = {
             type: 'text',
             text: event.message['text']
           }
-          client.reply_message(event['replyToken'], message)
-        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          #reply to user
+          if event.message['text'] == "gifteeのeチケットシェア率は？" then
+            reply_message = {
+              type: 'text',
+              text: "80%!"
+            }   
+          else
+            reply_message = {
+              type: 'text',
+              text: "わかりません。"
+            }
+          end
+          
+          client.reply_message(event['replyToken'], reply_message)
+        when Line::Bot::Event::MessageType::Image
+          puts event.message['originalContentUrl']
+
+
           response = client.get_message_content(event.message['id'])
           tf = Tempfile.open("content")
           tf.write(response.body)
+        else
+          #reply to user
+          reply_message = {
+            type: 'text',
+            text: "わかりません。"
+          }
+    client.reply_message(event['replyToken'], reply_message)    
+          
         end
       end
     }
